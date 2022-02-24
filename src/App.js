1a39839fad8e5c861e82c96798c22dfa22fd9bcf
Rio0628 <mariomdomenech@gmail.com>
2022-02-24 15:28:03 -0500

    Applied header elemnets and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React, { Component } from 'react';
+import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
+import { TiWeatherCloudy } from 'react-icons/ti';
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="container">
+        
+        <div className='headerBtnsContainer'>
+          <div className='openMenuBtn'><AiOutlineMenu className='logo'/></div>
+          <div className='searchbarCntr'>
+              <input className='searchbar' type='text' placeholder='Search Location'/>
+              <div className='searchBtn'><AiOutlineSearch className='logo'/></div>
+          </div> 
+        </div>
+
+        <div className='cntrOpenWeather'><div className='openWeatherBtn'><TiWeatherCloudy className='logo' /></div></div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
