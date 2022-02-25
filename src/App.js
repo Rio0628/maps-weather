9a2a7735777de4912d6552e9ae7f9cd95d5a9f69
Rio0628 <mariomdomenech@gmail.com>
2022-02-24 23:02:57 -0500

    Created map comp and basic map element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
 import { TiWeatherCloudy } from 'react-icons/ti';
+import Map from './components/Map';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: { latitude: 37.7577, longitude: -122.4376, width: window.innerWidth, height: window.innerHeight, zoom: 10},
+    }
+  }
+
+
   render () {
+
+
+    // mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaW9tZG9tZW5lY2giLCJhIjoiY2wwMXNqMzM4MHhlODNjbWx3aW95MTZqYiJ9.uVERxbdkPqvpiMcJLzimpQ'
+
     return (
       <div className="container">
-        
+        <Map />
+
         <div className='headerBtnsContainer'>
           <div className='openMenuBtn'><AiOutlineMenu className='logo'/></div>
           <div className='searchbarCntr'>
@@ -17,6 +31,7 @@ class App extends Component {
 
         <div className='cntrOpenWeather'><div className='openWeatherBtn'><TiWeatherCloudy className='logo' /></div></div>
 
+       
       </div>
     );
   }
---
+++ src/components/Map.js
@@ -0,0 +1,53 @@
+import React, { useRef, useEffect } from 'react';
+import ReactMapGl from 'react-map-gl';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const Map = () => {
+
+    const mapRef = useRef(null);
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaW9tZG9tZW5lY2giLCJhIjoiY2wwMXNqMzM4MHhlODNjbWx3aW95MTZqYiJ9.uVERxbdkPqvpiMcJLzimpQ'
+
+    // Initialize the map when comp mounts
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapRef.current,
+            style: 'mapbox://styles/mapbox/dark-v9',
+            center: [-104.9876, 39.7405],
+            zoom: 10
+        });
+
+        // Navigation controls
+        // map.addControl( new mapboxgl.NavigationControl(), 'bottom-right');
+    
+        // Clean up on unmount
+        return () => map.remove();
+    }, []);
+
+
+    return (
+        <div className='mapContainer'>
+            <div className='map' ref={mapRef}></div>
+        </div>
+    );
+}
+
+export default Map;
+
+/* 
+    const [ viewport, setViewport ] = useState({
+        width: window.innerWidth - 100,
+        height: window.innerHeight,
+        latitude: 25.761,
+        longitude: 80.1918,
+        zoom: 10,
+      });
+<ReactMapGl
+                className='map'
+                onViewportChange={nextViewport => setViewport(nextViewport)}
+                mapboxApiAccessToken={MAPBOX_TOKEN}
+                mapStyle='mapbox://styles/mapbox/dark-v9'
+                {...viewport}>
+            </ReactMapGl>
+*/
