6efcd9e289a75a200f7baf0acf2e129d2e28661a
Rio0628 <mariomdomenech@gmail.com>
2022-03-01 14:30:42 -0500

    Added basic search weather funct

--- src/App.js
+++ src/App.js
@@ -5,45 +5,75 @@ import { Map, SavedLocsCntr, CurrentWeather } from './components';
 import CurrentWeatherBG from './components/CurrentWeatherBG';
 import API from './api';
 import axios from 'axios';
+import gsap from 'gsap';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       viewport: { latitude: 37.7577, longitude: -122.4376, width: window.innerWidth, height: window.innerHeight, zoom: 10},
+      crrtWeatherSrchd: '',
+      inputSearchbar: '',
+      weatherSearched: false 
     }
+    this.weatherCntrAnim = gsap.timeline({ paused: true });
   }
 
-
+  
   render () {
 
-    console.log('mario domenech')
+    // console.log('mario domenech')
 
     // API.getAllLocs().then(data => console.log(data.data))
 
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=miami&appid=f0caa45808a9789d4f46776484b799e2&units=metric`).then(data => console.log(data))
+    // axios.get(`https://api.openweathermap.org/data/2.5/weather?q=miami&appid=f0caa45808a9789d4f46776484b799e2&units=metric`).then(data => console.log(data))
 
 
+    // console.log(this.state.crrtWeatherSrchd)
+
+    const searchWeather = async () => {
+      let lon, lat;
+      // let weatherCntrAnim = gsap.timeline({ paused: true });
+  
+      // Initial Axios call to retrieve lon and lat 
+      await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=miami&appid=f0caa45808a9789d4f46776484b799e2&units=metric`).then(data => {
+        lon = data.data.coord.lon;
+        lat = data.data.coord.lat;
+        // console.log(data.data)
+        this.setState({ currentWeatherName: `${data.data.name}, ${data.data.sys.country}`});
+      })
+    
+      await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&appid=4439be80c1f0ade164109e2399a51173
+      `).then(data => this.setState({ crrtWeatherSrchd: data.data }) );
+
+      // console.log(lon, lat);
+
+      this.setState({ weatherSearched: true });
+      this.setState({ weatherViewTrggrd: true });
+      this.weatherCntrAnim.play();
+    }
+console.log(this.state.crrtWeatherSrchd)
     return (
       <div className="container">
 
         <SavedLocsCntr />
 
         <div className='map-headerCntr'>
-          <Map />
+          {/* <Map /> */}
 
           <div className='headerBtnsContainer'>
             <div className='openMenuBtn'><AiOutlineMenu className='logo'/></div>
             <div className='searchbarCntr'>
-              <input className='searchbar' type='text' placeholder='Search Location'/>
-              <div className='searchBtn'><AiOutlineSearch className='logo'/></div>
+              <input className='searchbar' type='text' placeholder='Search Location' onChange={ (e) => this.setState({ inputSearchbar: e.target.value }) }/>
+              <div className='searchBtn' onClick={searchWeather}><AiOutlineSearch className='logo'/></div>
             </div> 
           </div>
 
-          <div className='cntrOpenWeather'><div className='openWeatherBtn'><TiWeatherCloudy className='logo' /></div></div>
+          <div className='cntrOpenWeather'><div className='openWeatherBtn' onClick={() => this.setState({ weatherSearched: true }) }><TiWeatherCloudy className='logo' /></div></div>
         </div>
 
-        <CurrentWeather  />
+        { this.state.weatherSearched ? 
+        <CurrentWeather locationName={this.state.currentWeatherName} weather={this.state.crrtWeatherSrchd} weatherSearched={this.state.weatherSearched} weatherAnim={this.weatherCntrAnim} /> : null }
 
 
       </div>
--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,92 +1,99 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { MdArrowBackIos } from 'react-icons/md';
 import { TiWeatherCloudy } from 'react-icons/ti';
 import CurrentWeatherBG from './CurrentWeatherBG';
+import gsap from 'gsap';
 
-const CurrentWeather = () => {
+const CurrentWeather = (props) => {
+
+    const cntrRef = useRef(null);
+    let cntrAnim = gsap.timeline({ paused: true });
+
+    useEffect(() => {
+        console.log(cntrRef)
+        props.weatherAnim.to(cntrRef.current, { opacity: 1, x: 0, duration: .5, ease: 'expo' })
+    }, []);
+
+    console.log(props.weather)
+    // const test = new Date(props.weather.current.dt * 1000);
+    // console.log(test)
+    // console.log(test.getHours() - 12)
+    const currentTime = new Date(props.weather.current.dt * 1000);
+
+    const getTime = (timeStamp) => {
+        let time = new Date(timeStamp * 1000);
+        time = time.getHours();
+
+        if (time === currentTime.getHours()) { return `Now`} 
+        else if (time > 12) { 
+            time = time - 12;
+            return `${time} PM`
+        }
+        else if (time === 12) { return `${time} PM`}
+        else if (time === 0) { return `12 AM`} 
+        else { return `${time} AM`}
+    }
+
+    const getDay = (date) => {
+        let day = new Date(date * 1000);
+        day = day.toString().split(' ');
+
+        return day[0]
+    }
+
+    const hourly = props.weather.hourly.slice(0, 24);
+    
+
     return (
-        <div className='currentWeatherCntr'>
+        <div className='currentWeatherCntr' ref={cntrRef}>
             <CurrentWeatherBG />
 
             <div className='wthrInfoCntr'>
-                <div className='returnMainViewBtn'  onClick={ () => console.log('mario')}>
+                <div className='returnMainViewBtn'  onClick={ () => props.weatherAnim.reverse()}>
                     <MdArrowBackIos className='logo'/>
 
                     <p className='returnBtnTxt'>Main View</p>
                 </div>
 
                 <div className='mainWthrLocCntr'>
-                    <p className='wthrLocName'>Location</p>
+                    <p className='wthrLocName'>{props.locationName}</p>
 
                     <div className='locCrrntWthr'><TiWeatherCloudy className='logo'/></div>
 
-                    <p className='crrntWthr'>00*C / 00*F</p>
+                    <p className='crrntWthr'>{props.weather.current.temp.toFixed()}°C</p>
 
-                    <p className='crrntWthrTxt'>Crrnt Weather</p>
+                    <p className='crrntWthrTxt'>{props.weather.current.weather[0].main}</p>
                     
                     <p className='saveWhtrLocBtn' onClick={ () => console.log('mario')}>Save Location</p>
                 </div>
             
                 <div className='wthrHrlCntr' id='wthrHrlCntr'>
-                    <div className='indHrlFrcst'>
-                        <div className='timeWthr'>00am</div>
-
-                        <div className='indHrlIcon'><TiWeatherCloudy className='logo' /></div>
-
-                        <p className='frcstTemp'>00*C</p>
-                    </div>
+                    { hourly.map(hour => 
+                        <div className='indHrlFrcst'>
+                          <div className='timeWthr'>{getTime(hour.dt)}</div>
+  
+                          <div className='indHrlIcon'><TiWeatherCloudy className='logo' /></div>
+  
+                    <p className='frcstTemp'>{hour.temp.toFixed()}°C</p>
+                        </div>
+                    )}
+                  
                 </div>
 
                 <p className='ftrFrcstHeading'>Future Forecast</p> 
 
                 <div className='ftrFrcstCntr'>
-                    <div className='indFrcst'>
-                        <p className='frcstDay'>Day</p>
+                    {props.weather.daily.map(day => 
+                        <div className='indFrcst'>
+                            <p className='frcstDay'>{getDay(day.dt)}</p>
 
-                        <div className='WthrIcon'><TiWeatherCloudy className='logo'/></div>
+                            <div className='WthrIcon'><TiWeatherCloudy className='logo'/></div>
 
-                        <p className='lowTemp'>00*C</p>
+                            <p className='lowTemp'>{day.temp.min.toFixed()}°C</p>
 
-                        <p className='highTemp'>00*C</p>
-                    </div>
-                    
-                    <div className='indFrcst'>
-                        <p className='frcstDay'>Day</p>
-
-                        <div className='WthrIcon'><TiWeatherCloudy className='logo'/></div>
-
-                        <p className='lowTemp'>00*C</p>
-
-                        <p className='highTemp'>00*C</p>
-                    </div>
-                    <div className='indFrcst'>
-                        <p className='frcstDay'>Day</p>
-
-                        <div className='WthrIcon'><TiWeatherCloudy className='logo'/></div>
-
-                        <p className='lowTemp'>00*C</p>
-
-                        <p className='highTemp'>00*C</p>
-                    </div>
-                    <div className='indFrcst'>
-                        <p className='frcstDay'>Day</p>
-
-                        <div className='WthrIcon'><TiWeatherCloudy className='logo'/></div>
-
-                        <p className='lowTemp'>00*C</p>
-
-                        <p className='highTemp'>00*C</p>
-                    </div>
-                    <div className='indFrcst'>
-                        <p className='frcstDay'>Day</p>
-
-                        <div className='WthrIcon'><TiWeatherCloudy className='logo'/></div>
-
-                        <p className='lowTemp'>00*C</p>
-
-                        <p className='highTemp'>00*C</p>
-                    </div>
-                    
+                            <p className='highTemp'>{day.temp.max.toFixed()}°C</p>
+                        </div>
+                    )}
                 </div>
             </div>
            
