3bd4c99e9cff7acb8b4df7afad3bc75394b5cc0d
Rio0628 <mariomdomenech@gmail.com>
2022-03-08 11:03:54 -0500

    Made changes to production side

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
 
+const URL = process.env.API_URL || 'http://localhost:8080';
+
 const http = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: URL,
     headers: { 'Content-type': 'application/json'}
 })
 
-export const createLoc = data => http.post('/locations', data);
-export const getAllLocs = () => http.get('/locations');
-export const deleteLoc = id => http.delete(`/locations/${id}`);
-export const updateLoc = (id, data) => http.put(`/locations/${id}`, data);
+export const createLoc = data => http.post('/api/locations', data);
+export const getAllLocs = () => http.get('/api/locations');
+export const deleteLoc = id => http.delete(`/api/locations/${id}`);
+export const updateLoc = (id, data) => http.put(`/api/locations/${id}`, data);
     
 const apis = { createLoc, getAllLocs, deleteLoc, updateLoc }; 
 
