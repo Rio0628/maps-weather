158acc7b66c0cdf3c66c344aae05cb75952a3d24
Rio0628 <mariomdomenech@gmail.com>
2022-03-08 11:52:04 -0500

    Updated router methods

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,9 @@
 import axios from 'axios';
 
-const URL = process.env.API_URL || 'http://localhost:8080';
-
-const http = axios.create({
-    baseURL: URL,
-    headers: { 'Content-type': 'application/json'}
-})
-
-export const createLoc = data => http.post('/api/locations', data);
-export const getAllLocs = () => http.get('/api/locations');
-export const deleteLoc = id => http.delete(`/api/locations/${id}`);
-export const updateLoc = (id, data) => http.put(`/api/locations/${id}`, data);
+export const createLoc = data => axios.post('/api/locations', data);
+export const getAllLocs = () => axios.get('/api/locations');
+export const deleteLoc = id => axios.delete(`/api/locations/${id}`);
+export const updateLoc = (id, data) => axios.put(`/api/locations/${id}`, data);
     
 const apis = { createLoc, getAllLocs, deleteLoc, updateLoc }; 
 
